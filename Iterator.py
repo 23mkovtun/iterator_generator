ad2d28b77044dae855e5b0c069093799ba3d9f1d
Mari test <23mkovtun@mail.ru>
2023-08-18 09:56:56 +0300

    First commit

--- Iterator.py
+++ Iterator.py
@@ -1,49 +1,41 @@
 class FlatIterator:
 
-    def __init__(self, multi_list):
-        self.multi_list = multi_list
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
 
     def __iter__(self):
-        self.list_iter = iter(self.multi_list)  
-        self.nested_list = []  
-        self.cursor = -1  
+        self.list_iterator = iter(self.list_of_list)
+        self.list_ = []
+        self.cur = -1
         return self
 
     def __next__(self):
-        self.cursor += 1
-        if len(self.nested_list) == self.cursor: 
-            self.nested_list = None
-            self.cursor = 0
-            while not self.nested_list:  
-                self.nested_list = next(self.list_iter)  
-        return self.nested_list[self.cursor]
+        self.cur += 1
+        if len(self.list_) == self.cur:
+            self.list_ = None
+            self.cur = 0
+            if self.list_ is None:
+                self.list_ = next(self.list_iterator)
+        return self.list_[self.cur]
 
 
-def flat_generator(my_list):
-    for sub_list in my_list:
-        for elem in sub_list:
-            yield elem
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    print(list(FlatIterator(list_of_lists_1)))
 
 
 if __name__ == '__main__':
-
-    nested_list = [
-        ['a', 'b', 'c'],
-        ['d', 'e', 'f', 'h', False],
-        [1, 2, None],
-    ]
-
-
-    print('итератор')
-    for item in FlatIterator(nested_list):
-        print(item)
-    
-
-    print('компрехеншен')
-    flat_list = [item for item in FlatIterator(nested_list)]
-    print(flat_list)
-
-
-    print('генератор')
-    for item in flat_generator(nested_list):
-        print(item)
+    test_1()
